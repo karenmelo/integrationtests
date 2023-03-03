b3d622a8f88e93aa01ef87a697a945d03755934a
Karen Melo <karen_melo@live.com>
2023-03-03 19:15:08 -0300

    Inclusao de 3 projetos base para a parte de vendas do web app para realizar os testes de integracao

---
+++ src/NerdStore.WebApp.MVC/Controllers/CartControllerAPI.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NerdStore.WebApp.MVC.Controllers
+{
+    public class CartControllerAPI : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/NerdStore.WebApp.MVC/Controllers/ShowcaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NerdStore.WebApp.MVC.Controllers
+{
+    public class ShowcaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
